45e03d31131a131bf30553e5e32aec4ce3874f4a
GopisettyBhavya <bhavyagopisetty06@gmail.com>
2025-07-28 20:40:42 +0530

    Day 1 Completed

---
+++ lc_01_1768.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder merged = new StringBuilder();
+        int length = Math.max(word1.length(), word2.length());
+        for(int i=0; i<length; i++) {
+            if(i < word1.length()) {
+                   merged.append(word1.charAt(i));  
+            } 
+            if(i < word2.length()) {
+                merged.append(word2.charAt(i));
+            }
+        }
+        return merged.toString();
+    }
+    public static void main (String args[]) {
+        System.out.println(mergeAlternately("abc", "pqr"));
+        System.out.println(mergeAlternately("ab", "pqrs"));
+        System.out.println(mergeAlternately("abcd", "pq"));
+    }
+}
